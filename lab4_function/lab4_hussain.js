6606023bce266862b8e26c8004286b7e83c1a71a
mr-a006 <aqeel.hussain14@student.qcc.cuny.edu>
2025-09-09 15:42:34 -0400

    lab4 WIP

---
+++ lab4_function/lab4_hussain.js
@@ -0,0 +1,61 @@
+/*
+Aqeel Hussain
+Lab 4, Fucntions
+September 9, 2025
+ */
+console.log("Aqeel Hussain")
+// define a function to print a message
+function msg(){
+    console.log("----- Example 1 -----")
+    console.log("Hello World!")
+}
+
+// define a function to print from 3 to 1
+function printCount(){
+    console.log("----- Example 2 -----")
+    for(let n = 3; n>0; n--){
+        console.log(n)
+    }
+}
+
+// define a function to passed a username
+function greeting(username){
+    console.log("----- Example 3 -----")
+    console.log(`Welcome to JS ${username}`)
+}
+
+// define a function passses a username and print it in uppercase
+function upperMessage(message){
+    console.log("----- Example 4 -----")
+    let changeUpper = message.toUpperCase()
+    console.log(changeUpper)
+}
+
+// define a function to simulate a snake eyes game.
+// pass two numbers to the function, and check if both numbers are '1'
+function isSnakeEyes(dice1, dice2){
+    console.log("----- Example 5 -----")
+    if(dice1===1 && dice2 === 1){
+        console.log("Snake Eyes!")
+    }
+    else{
+        console.log("Not Snake Eyes!")
+    }
+}
+
+// define a function that returns the area of a rectangle
+function areaRectangle(length, width){
+    console.log("----- Example 6 -----")
+    return length*width
+}
+
+// define a function to check a temperature. If the temperature is greater than 75, returns 'true', otherwise, it returns a 'false'
+function checkTemperature(temperature){
+    if (temperature>=75){
+        return true
+    }
+    else{
+        return false
+    }
+}
+//EXERCISE
