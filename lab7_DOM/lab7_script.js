397190ae4af1bfb9b599952c487a072e76000749
mr-a006 <aqeel.hussain14@student.qcc.cuny.edu>
2025-09-30 15:33:11 -0400

    lab7 events in JS

--- lab7_DOM/lab7_script.js
+++ lab7_DOM/lab7_script.js
@@ -45,4 +45,60 @@ btn_rectangle.addEventListener("click", function(){
     shape.className = "rectangle"
     shape.textContent = "rectangle".toUpperCase()
     shape.style.backgroundColor = "gray";
+    shape.style.fontSize = "2rem"
 } )
+
+// SEP 30
+// event handler
+let btnpress = document.querySelector(".btnpress")
+btnpress.onclick = function(){alert(`${new Date()}`)}
+
+// mouse events
+let x = document.querySelector(".x")
+let circle_paragraph = document.querySelector(".circle_paragraph")
+x.onmouseover = function(){circle_paragraph.textContent += "- CIRCLE - "}
+
+// event listener
+// collect the element
+let box_color = document.querySelector(".box_color")
+// function to randomly pick a color as a rgb value
+const randomRGB = function(){
+    let red = Math.floor(Math.random()*255)
+    let green = Math.floor(Math.random()*255)
+    let blue = Math.floor(Math.random()*255)
+    return `rgb(${red}, ${green}, ${blue})`
+}
+box_color.addEventListener("mouseout",function(){
+    box_color.style.backgroundColor = randomRGB()
+})
+
+// key event
+// collect the element
+let inputtext = document.querySelector(".inputtext")
+let username_paragraph = document.querySelector(".username_paragraph")
+inputtext.addEventListener("keydown", function(){
+    username_paragraph.textContent = "Username MUST have 3+ characters"
+})
+
+// LAB EXERCISE
+// note: use querySelector() or querySelectorAll()
+// collect elements: search for the first p with class ps1
+
+// add a mouseout event to change the text
+
+
+// search for the second p with class ps2
+
+
+// add a click event to the ps2 to change the
+
+
+// search the first div with class divs2
+
+
+// add a click event to change the background
+
+
+// search for the second div with class divs2
+
+// add a keydown event to change the width of
