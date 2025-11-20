461ed1a5caaa1363b873dc961c0648a5c4d4b187
mr-a006 <aqeelhussain2006.02@gmail.com>
2025-11-20 20:48:00 +0000

    state form || lab 15 wip 2

--- lab15_form_state/src/App.js
+++ lab15_form_state/src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
+function App(){
+  /*
+  // set initial state for username
+  const [username, setUsername] = useState("")
+  */
+
+  // set states for multiple inputs
+  const [inputs, setInputs] = useState({})
+  // function to handle changes in all inputs
+  const handle_changes = function(event){
+    const name = event.target.name
+    const value = event.target.value
+    // update the states
+    setInputs(values =>({...values, [name]: value}))
+  }
+
+  // set states for select gender
+  const [myGender, setMygender] = useState("")
+  // function to collect the selected gender
+  const selectedGender = (event)=>{
+    setMygender(event.target.value)
+  }
+
+  // set state for comments
+  const [textcomment, setComment] = useState("")
+  // function to collect the comments
+  const submitted_comments = function(event){
+    setComment(event.target.value)
+  }
+
+  // function submit form
+  const submitform = function(event){
+    event.preventDefault()
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='myform' onSubmit={submitform}>
+        <fieldset>
+          <legend>Forms in React JS</legend>
+          <div className='formgroup'>
+            <label for="name">Enter username: </label>
+            <input
+              type='text'
+              id='name'
+              className='inputstyle'
+              name='username'
+              placeholder='Type the username...'
+              value={inputs.username}
+              onChange={handle_changes}
+            />
+          </div>
+
+          <div className='formgroup'>
+            <label for="age">Enter your age: </label>
+            <input
+              type='number'
+              id='age'
+              className='inputstyle'
+              name='age'
+              placeholder='Type in your age number...'
+              value={inputs.age}
+              onChange={handle_changes}
+            />
+          </div>
+        
+        {/** select a gender */}
+        <div className='formgroup'>
+          <label> Select a gender:
+            <select value={myGender} onChange={selectedGender}>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+              <option value="other">Other</option>
+            </select>
+          </label>
+        </div>
+
+
+        {/** comments using textarea */}
+        <div className='formgroup'>
+          <label for="comments">Any suggestions? </label>
+          <textarea
+            id='comments'
+            value={textcomment}
+            onChange={submitted_comments}
+            placeholder='Please type your comments'
+          />
+        </div>
+
+
+        {/** submit form */}
+        <input type='submit' className='btnsubmit'/>
+        </fieldset>
+      </form>
+      {/** test the username collected after the submit button */}
+      <p className='printresult'>Username = {inputs.username}</p>
+      <p className='printresult'>Age = {inputs.age}</p>
+      <p className='printresult'>Gender = {myGender}</p>
+      <p className='printresult'>Comments = {textcomment}</p>
+      
+
     </div>
   );
 }
