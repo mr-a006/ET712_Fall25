783ff59652c3de8d788a3b69c9b8db8e28031559
mr-a006 <aqeel.hussain14@student.qcc.cuny.edu>
2025-12-04 14:24:15 -0500

    lab 17 wip 2

--- lab17_nodejs/lab17.js
+++ lab17_nodejs/lab17.js
@@ -1,2 +1,14 @@
 console.log("Aqeel Hussain")
+console.log('------ Example 1: console -----')
 console.warn("Warning! check the last line before proceed!")
+console.error("ERROR, port is not available!")
+console.trace("TRACE LOCATION")
+
+setTimeout(()=>{
+    console.warn("Warning! input string for the next line")
+}, 3000)
+
+console.log('------ Example 2: creating a simple module file -----')
+// const name = require("./mod.js")
+import helper from "./mod.js"
+console.log(helper("Peter"))
