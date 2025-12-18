8f145f9288ae5f66721822a2570f3455b173a5d0
mr-a006 <aqeelhussain2006.02@gmail.com>
2025-12-18 19:45:49 +0000

    prices added

--- project2/src/pages/Category1.js
+++ project2/src/pages/Category1.js
@@ -3,14 +3,14 @@ import Card from '../components/Card';
 
 function Category1({ addToCart }) {
   const products = [
-    { id: 1, title: 'Sony Wireless Headphones', price: 89, image: '/img/sonywireless.jpg', description: 'Featuring excellent sound quality and extra bass without the hassle of wires, these headphones are perfect for everyday use.' },
-    { id: 2, title: 'Sony Wired Headphones', price: 49, image: '/img/sonywired.jpg', description: 'For a more classic style of headphones, offering the same great sound quality and bass that has made Sony well respected.' },
-    { id: 3, title: 'Logitech Web Camera', price: 79, image: '/img/logitechcamera.jpg', description: 'Utilize your meetings with the Logitech Web Camera. Featuring clear video and sound, this surely adds new life to network meetings.' },
-    { id: 4, title: 'HyperX Microphone', price: 99, image: '/img/hyperxmicrophone.jpg', description: 'The HyperX Microphone delivers extremely clear sound quality, and the best part is that it has very little static or background noise.' },
-    { id: 5, title: 'Logitech Keyboard', price: 69, image: '/img/logitechkeyboard.jpg', description: 'Whether you are sending a quick e-mail or working on a college research paper, the quiet Logitech Keyboard helps you get the task done right.' },
-    { id: 6, title: 'Dell Monitor', price: 199, image: '/img/dellmonitor.jpg', description: 'To see the picture, you need a monitor that delivers sharp pixels and smooth performance, and the Dell Monitor does exactly that.' },
-    { id: 7, title: 'Logitech Wireless Mouse', price: 59, image: '/img/logitechwireless.jpg', description: 'Almost everyone else rely on the comfort and performance of the Logitech Wireless Mouse, being able to plug and play without the hassle of wires.' },
-    { id: 8, title: 'Logitech Wired Mouse', price: 39, image: '/img/logitechwired.jpg', description: 'The classic Logitech Wired Mouse remains timeless, offering the same comfort and performance that has made Logitech well respected.' },
+    { id: 1, title: 'Sony Wireless Headphones', price: 89, image: '/img/sonywireless.jpg', description: 'Featuring excellent sound quality and extra bass without the hassle of wires, these headphones are perfect for everyday use. $89' },
+    { id: 2, title: 'Sony Wired Headphones', price: 49, image: '/img/sonywired.jpg', description: 'For a more classic style of headphones, offering the same great sound quality and bass that has made Sony well respected. $49' },
+    { id: 3, title: 'Logitech Web Camera', price: 79, image: '/img/logitechcamera.jpg', description: 'Utilize your meetings with the Logitech Web Camera. Featuring clear video and sound, this surely adds new life to network meetings. $79' },
+    { id: 4, title: 'HyperX Microphone', price: 99, image: '/img/hyperxmicrophone.jpg', description: 'The HyperX Microphone delivers extremely clear sound quality, and the best part is that it has very little static or background noise. $99' },
+    { id: 5, title: 'Logitech Keyboard', price: 69, image: '/img/logitechkeyboard.jpg', description: 'Whether you are sending a quick e-mail or working on a college research paper, the quiet Logitech Keyboard helps you get the task done right. $69' },
+    { id: 6, title: 'Dell Monitor', price: 199, image: '/img/dellmonitor.jpg', description: 'To see the picture, you need a monitor that delivers sharp pixels and smooth performance, and the Dell Monitor does exactly that. $199' },
+    { id: 7, title: 'Logitech Wireless Mouse', price: 59, image: '/img/logitechwireless.jpg', description: 'Almost everyone else rely on the comfort and performance of the Logitech Wireless Mouse, being able to plug and play without the hassle of wires. $59' },
+    { id: 8, title: 'Logitech Wired Mouse', price: 39, image: '/img/logitechwired.jpg', description: 'The classic Logitech Wired Mouse remains timeless, offering the same comfort and performance that has made Logitech well respected. $39' },
   ];
 
   return (
--- project2/src/pages/Category2.js
+++ project2/src/pages/Category2.js
@@ -3,14 +3,14 @@ import Card from '../components/Card';
 
 function Category2({ addToCart }) {
   const products = [
-    { id: 10, title: 'Dell Computer Case', price: 129, image: '/img/dellcase.jpg', description: 'House and protect your computer with this reliable computer case.  The Dell computer case is durable, well-built, and secure.' },
-    { id: 11, title: 'Intel i9 14th Generation CPU', price: 549, image: '/img/intelcpu.jpg', description: 'A powerful CPU will make daily tasks faster, easier, and simpler for you  Enjoy fast performance and smooth multitasking.' },
-    { id: 12, title: 'Nvidia RTX 4080 GPU', price: 1199, image: '/img/rtx4080.jpg', description: 'A high-performance graphics card will enhance your gaming experience. Enjoy high frames and response times.' },
-    { id: 13, title: 'Gigabyte Motherboard', price: 249, image: '/img/gigabyteboard.jpg', description: 'Repair or upgrade your computer with a brand new motherboard.  The motherboards meet industry standards and provide great performance.' },
-    { id: 14, title: 'Corsair Power Supply', price: 179, image: '/img/corsairpower.jpg', description: 'Power your computer reliably with this long-lasting power supply. A high-quality power supply that provides long-time power.' },
-    { id: 15, title: '16GB Memory Stick', price: 79, image: '/img/16gbmemory.jpg', description: 'Boost your computer with this 16GB memory stick. Your computer will run smoother and faster, having more room for a variety of tasks.' },
-    { id: 16, title: 'Samsung 1TB SSD', price: 129, image: '/img/samsungssd.jpg', description: 'Expand the storage of your computer with this state of the art SSD, utilizing more performance, power, and features.' },
-    { id: 17, title: 'Noctua Computer Fan', price: 39, image: '/img/noctuafan.jpg', description: 'Help keep your computer cool and operating smoothly with a computer fan. This fan can reliably balance everything in your computer.' },
+    { id: 10, title: 'Dell Computer Case', price: 129, image: '/img/dellcase.jpg', description: 'House and protect your computer with this reliable computer case.  The Dell computer case is durable, well-built, and secure. $129' },
+    { id: 11, title: 'Intel i9 14th Generation CPU', price: 549, image: '/img/intelcpu.jpg', description: 'A powerful CPU will make daily tasks faster, easier, and simpler for you  Enjoy fast performance and smooth multitasking. $549' },
+    { id: 12, title: 'Nvidia RTX 4080 GPU', price: 1199, image: '/img/rtx4080.jpg', description: 'A high-performance graphics card will enhance your gaming experience. Enjoy high frames and response times. $1199' },
+    { id: 13, title: 'Gigabyte Motherboard', price: 249, image: '/img/gigabyteboard.jpg', description: 'Repair or upgrade your computer with a brand new motherboard.  The motherboards meet industry standards and provide great performance. $249' },
+    { id: 14, title: 'Corsair Power Supply', price: 179, image: '/img/corsairpower.jpg', description: 'Power your computer reliably with this long-lasting power supply. A high-quality power supply that provides long-time power. $179' },
+    { id: 15, title: '16GB Memory Stick', price: 79, image: '/img/16gbmemory.jpg', description: 'Boost your computer with this 16GB memory stick. Your computer will run smoother and faster, having more room for a variety of tasks. $79' },
+    { id: 16, title: 'Samsung 1TB SSD', price: 129, image: '/img/samsungssd.jpg', description: 'Expand the storage of your computer with this state of the art SSD, utilizing more performance, power, and features. $129' },
+    { id: 17, title: 'Noctua Computer Fan', price: 39, image: '/img/noctuafan.jpg', description: 'Help keep your computer cool and operating smoothly with a computer fan. This fan can reliably balance everything in your computer. $39' },
   ];
 
   return (
